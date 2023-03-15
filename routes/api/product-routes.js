a3063ad26c39f0f0323f7666fea1c5a809d87b7a
Jared Tichacek <jared.tichacek@gmail.com>
2023-03-15 17:33:11 -0500

    cleaned up code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
@@ -20,31 +19,23 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
-  const productData = await Product.findByPk(req.params.id, {
-    include: [{ model: Category }, {model: Tag }],
-  })
-  if(!productData) {
-    res.status(404).json({ message: 'No product found by this ID!' })
-    return
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    })
+    if (!productData) {
+      res.status(404).json({ message: 'No product found by this ID!' })
+      return
+    }
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
   }
-  res.status(200).json(productData)
-} catch (err) {
-  res.status(500).json(err)
-}
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -114,8 +105,8 @@ router.delete('/:id', async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: {
-        id: req.params.id, 
-      }, 
+        id: req.params.id,
+      },
     })
     if (!productData) {
       res.status(404).json({ message: 'No product found by this ID!' })
