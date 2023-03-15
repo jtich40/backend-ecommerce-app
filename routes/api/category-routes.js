5049e15d9df0c6f5cb478da395cefb279ce74b96
Jared Tichacek <jared.tichacek@gmail.com>
2023-03-15 14:54:36 -0500

    fixed POST and PUT routes in category-routes.js and tag-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,9 +36,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create({
-      category_id: req.body.category_id,
-    })
+    const categoryData = await Category.create(req.body)
     res.status(200).json(categoryData)
   } catch (err) {
     res.status(400).json(err)
@@ -53,7 +51,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     })
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category found by this ID! '})
       return
     }
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,9 +36,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create({
-      tag_id: req.body.tag_id,
-    })
+    const tagData = await Tag.create(req.body)
     res.status(200).json(tagData)
   } catch (err) {
     res.status(400).json(err)
@@ -53,7 +51,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     })
-    if (!tagData[0]) {
+    if (!tagData) {
       res.status(404).json({ message: 'No tag found by this ID!' })
       return
     }
